8bef595755d7821932ad3b59e7960d1f3a194cd7
Raphael Pinto <raphaelcpinto@gmail.com>
2022-03-22 12:09:53 -0300

    feat: add gin-monitor

--- adapter.go
+++ adapter.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/bancodobrasil/featws-resolver-adapter-go/routes"
 	"github.com/bancodobrasil/featws-resolver-adapter-go/services"
+	ginMonitor "github.com/bancodobrasil/gin-monitor"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -27,9 +29,19 @@ func Run(resolverFunc services.ResolverFunc, config Config) error {
 
 	setupLog()
 
+	monitor, err := ginMonitor.New("v0.0.1-rc6", ginMonitor.DefaultErrorMessageKey, ginMonitor.DefaultBuckets)
+	if err != nil {
+		panic(err)
+	}
+
 	services.SetupResolver(resolverFunc)
 
 	router := gin.New()
+
+	// Register gin-monitor middleware
+	router.Use(monitor.Prometheus())
+	// Register metrics endpoint
+	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
 	routes.SetupRoutes(router)
 
