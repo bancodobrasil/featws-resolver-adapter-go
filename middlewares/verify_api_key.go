60b196e3db1eee94c45f6fdbfb5358f3aa4c3e81
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-10-14 15:38:10 -0300

    fix: golint

--- middlewares/verify_api_key.go
+++ middlewares/verify_api_key.go
@@ -5,25 +5,33 @@ import (
 	"github.com/spf13/viper"
 )
 
+// VerifyAPIKeyMiddleware ...
 type VerifyAPIKeyMiddleware struct {
 	key string
 }
 
 var verifyAPIKeyMiddleware *VerifyAPIKeyMiddleware
 
-// Middleware function to verify the JWT token
+// VerifyAPIKey ...
 func VerifyAPIKey() gin.HandlerFunc {
 	return verifyAPIKeyMiddleware.Run()
 }
 
+// NewVerifyAPIKeyMiddleware ...
 func NewVerifyAPIKeyMiddleware() {
 	verifyAPIKeyMiddleware = &VerifyAPIKeyMiddleware{
 		key: viper.GetString("RESOLVER_API_KEY"),
 	}
 }
 
+// Run ...
 func (m *VerifyAPIKeyMiddleware) Run() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		if m.key == "" {
+			c.Next()
+			return
+		}
+
 		key := m.extractKeyFromHeader(c)
 
 		if key != m.key {
